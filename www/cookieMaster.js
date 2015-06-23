7638286176d6891c055277b9f5a0b876193bb47c
Kyle Yetter <kyetter@ebackpack.com>
2015-06-23 11:16:23 -0400

    Refactor JS plugin frontend to allow a cookie options parameter. #PAR-95

--- www/cookieMaster.js
+++ www/cookieMaster.js
@@ -1,17 +1,55 @@
-var cookieMaster = {
-  getCookieValue: function(url, cookieName, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'CookieMaster', 'getCookieValue', [url, cookieName]);
-  },
-  setCookieValue: function (url, cookieName, cookieValue) {
+(function(mod) {
+  'use strict';
+
+  var cookieMaster = {
+    getCookieValue: getCookieValue,
+    setCookieValue: setCookieValue
+  };
+
+  mod.exports = cookieMaster;
+
+  return cookieMaster;
+
+  function getCookieValue(url, cookieName, onSuccess, onError) {
     cordova.exec(
-      function () {
-        console.log('New Cookie has been set');
-      },
-      function (err) {
-        console.log('Something went wrong setting the new cookie: ', err);
-      },
-      'CookieMaster', 'setCookieValue', [url, cookieName, cookieValue]
+      onSuccess,
+      onError,
+      'CookieMaster',
+      'getCookieValue',
+      [url, cookieName]
     );
   }
-};
-module.exports = cookieMaster;
+
+  function setCookieValue(url, cookieName, cookieValue, cookieOptions, onSuccess, onError) {
+    cookieOptions = cookieOptions || {};
+    onError       = onError       || noop;
+    onSuccess     = onSuccess     || noop;
+
+    if (typeof onError !== 'function') {
+      console.log("CookieMaster.setCookieValue failure: onError parameter not a function");
+      return;
+    }
+
+    if (typeof onSuccess !== 'function') {
+      return onError("CookieMaster.setCookieValue failure: onSuccess parameter not a function");
+    }
+
+    if (!isObject(cookieOptions)) {
+      return onError("CookieMaster.setCookieValue failure: cookieOptions parameter not an object");
+    }
+
+    cordova.exec(
+      onSuccess,
+      onError,
+      'CookieMaster',
+      'setCookieValue',
+      [url, cookieName, cookieValue, cookieOptions]
+    );
+  }
+
+  function isObject(value) {
+    return value != null && typeof value === 'object';
+  }
+
+  function noop() { }
+})(module);
