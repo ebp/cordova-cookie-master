63b8514c860199ff73743e28c1f8c5fbc88f6ba2
Kyle Yetter <kyetter@ebackpack.com>
2015-06-23 14:52:40 -0400

    Update Android w/ cookie options object parameter. #PAR-95

--- src/android/com/cordova/plugins/cookiemaster/CookieMaster.java
+++ src/android/com/cordova/plugins/cookiemaster/CookieMaster.java
@@ -1,5 +1,5 @@
 package com.cordova.plugins.cookiemaster;
- 
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -20,65 +20,83 @@ public class CookieMaster extends CordovaPlugin {
   private final String TAG = "CookieMasterPlugin";
   public static final String ACTION_GET_COOKIE_VALUE = "getCookieValue";
   public static final String ACTION_SET_COOKIE_VALUE = "setCookieValue";
-  
+
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     try {
       if (ACTION_GET_COOKIE_VALUE.equals(action)) {
         final String url = args.getString(0);
         final String cookieName = args.getString(1);
-        
+
         cordova.getThreadPool().execute(new Runnable() {
           public void run() {
             try {
               CookieManager cookieManager = CookieManager.getInstance();
-              String[] cookies = cookieManager.getCookie(url).split("; ");
-              String cookieValue = "";
+              String fullCookieString = cookieManager.getCookie(url);
+              if (fullCookieString == null) {
+                fullCookieString = "";
+              }
+              String[] cookies = fullCookieString.split("; ");
+              String cookieValue = null;
 
               for (int i = 0; i < cookies.length; i++) {
-                if (cookies[i].contains(cookieName + "=")) {
+                String currentCookie = cookies[i].trim();
+                if (currentCookie.matches("^\\s*\\Q" + cookieName + "\\E\\s*=\\s*")) {
                   cookieValue = cookies[i].split("=")[1].trim();
                   break;
                 }
               }
-              
-              JSONObject json = null;
-              if (cookieValue != "") {
-                json = new JSONObject("{cookieValue:\"" + cookieValue + "\"}");
-              }
-              if (json != null) {
-                PluginResult res = new PluginResult(PluginResult.Status.OK, json);
-                callbackContext.sendPluginResult(res);        
+
+              if (cookieValue != null) {
+                PluginResult res = new PluginResult(PluginResult.Status.OK, cookieValue);
+                callbackContext.sendPluginResult(res);
               } else {
                 callbackContext.error("Cookie not found!");
               }
             } catch (Exception e) {
               Log.e(TAG, "Exception: " + e.getMessage());
               callbackContext.error(e.getMessage());
-            }        
+            }
           }
         });
-        return true;      
+        return true;
       } else if (ACTION_SET_COOKIE_VALUE.equals(action)) {
         final String url = args.getString(0);
         final String cookieName = args.getString(1);
         final String cookieValue = args.getString(2);
+        final JSONObject cookieOptions = args.getJSONObject(3);
 
         cordova.getThreadPool().execute(new Runnable() {
           public void run() {
             try {
               HttpCookie cookie = new HttpCookie(cookieName, cookieValue);
 
+              if (cookieOptions.has("path")) {
+                cookie.setPath(cookieOptions.getString("path"));
+              }
+
+              if (cookieOptions.has("domain")) {
+                cookie.setDomain(cookieOptions.getString("domain"));
+              }
+
+              if (cookieOptions.has("maxAge")) {
+                cookie.setMaxAge(cookieOptions.getLong());
+              }
+
+              if (cookieOptions.has("secure")) {
+                cookie.setSecure(cookieOptions.getBoolean());
+              }
+
               String cookieString = cookie.toString().replace("\"", "");
               CookieManager cookieManager = CookieManager.getInstance();
               cookieManager.setCookie(url, cookieString);
-              
+
               PluginResult res = new PluginResult(PluginResult.Status.OK, "Successfully added cookie");
-              callbackContext.sendPluginResult(res);        
+              callbackContext.sendPluginResult(res);
             } catch (Exception e) {
               Log.e(TAG, "Exception: " + e.getMessage());
               callbackContext.error(e.getMessage());
-            }        
+            }
           }
         });
         return true;
